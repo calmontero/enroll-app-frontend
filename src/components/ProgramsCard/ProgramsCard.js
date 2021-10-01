747570717950f39f31925323e21a52536de0c239
Carlos <carlos.alfonso.montero@gmail.com>
2021-09-30 21:59:58 -0400

    added programs design

--- src/components/ProgramsCard/ProgramsCard.js
+++ src/components/ProgramsCard/ProgramsCard.js
@@ -1,24 +1,43 @@
 import React from "react";
 import './ProgramsCard.css';
 import { Link } from 'react-router-dom';
+import Typography from '@material-ui/core/Typography';
+import { ImClock, ImSphere } from "react-icons/im";
+import Button from '@material-ui/core/Button';
+import Rating from '@material-ui/lab/Rating';
+import Box from '@material-ui/core/Box';
 
 function ProgramsCard({ programs }) {
     const {id, name, summary, hours, image} = programs;
 
     return (
         <div className= "program-card">
-            
-            <img
-                className="program-img"
-                src={image}
-                alt={name}
-                onError={(event) => event.target.style.display = 'none'}
-                style={{borderTopRightRadius: 20, borderBottomRightRadius: 20, borderBottomLeftRadius: 20, borderTopLeftRadius: 20}}
-            />
-            <h2>
-                <Link to={`/programs/${id}`}>{name}</Link>
-            </h2>
-            <h3>Summary: {summary}</h3>
+            <div className="flex-child magenta" key={id} >
+                <img
+                    className="program-img"
+                    src={image}
+                    alt={name}
+                    onError={(event) => event.target.style.display = 'none'}
+                    style={{borderTopRightRadius: 20, borderBottomRightRadius: 20, borderBottomLeftRadius: 20, borderTopLeftRadius: 20}}
+                />
+            </div>
+            <div className="flex-child1 green">
+                <Typography variant="h4" color="primary" >
+                        {name}
+                </Typography>
+                <Box component="fieldset" mb={3} borderColor="transparent">
+                    <Rating name="read-only" value="4" readOnly />
+                </Box>
+                <Typography variant="h5">
+                    {summary}
+                </Typography>
+                <br/>
+                <h5><ImClock /> Approx. {hours} hours to complete</h5>
+                <h5><ImSphere /> 100% online</h5>
+                <Link to={`/programs/${id}`}>
+                    <Button type="submit"variant="outlined" color="primary">Enroll</Button>
+                </Link>
+            </div>
         </div>
     )
 }
--- src/components/ProgramsContainer/ProgramsContainer.js
+++ src/components/ProgramsContainer/ProgramsContainer.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ProgramsCard from "../ProgramsCard/ProgramsCard";
+import Typography from '@material-ui/core/Typography';
 import { BASE_URL } from "../../constraints";
 
 function ProgramsContainer() {
@@ -17,8 +18,10 @@ function ProgramsContainer() {
     }, []);
       
       return (
-        <div className="programs-container">
-            <h1>Programs</h1>
+        <div className="programs-container" >
+            <Typography variant="h2" component="h2" gutterBottom color= "primary" align="center">
+                Programs
+            </Typography>
             {
                 programs && programs.length > 0 && programs.map(p => {
                     return <ProgramsCard
